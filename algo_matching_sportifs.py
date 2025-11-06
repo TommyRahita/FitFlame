ba40aaaee4235ff0955b55908fb790196ac3f47e
ythollet <159007949+ythollet@users.noreply.github.com>
2025-11-06 17:09:34 +0100

    algo_matching_sportifs

---
+++ algo_matching_sportifs.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import json
+import math
+
+
+
+chemin_actuel = os.getcwd()
+sys.path.append(chemin_actuel)
+
+from class_sportif import *
+
+def calculer_distance_haversine(lat1, lon1, lat2, lon2):
+    """
+    Calcule la distance Haversine (grand cercle) entre deux points GPS.
+    Les coordonnées doivent être en degrés décimaux.
+    Retourne la distance en kilomètres (km).
+    """
+    R = 6371  # Rayon de la Terre en kilomètres
+    
+    # 1. Conversion des degrés en radians
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    # 2. Différences de coordonnées
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+
+    # 3. Application de la formule de Haversine
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    
+    distance = R * c
+    return distance
+
+
+def func_algo_matching(
+    # sportif_a_matcher: Sportif
+):
+
+    with open("data.json", 'r', encoding='utf-8') as fichier:
+        liste_sportifs = json.load(fichier)
+
+    for dict_sportif in liste_sportifs:
+        current_sportif = Sportif(
+            nom = dict_sportif['nom'],
+            prenom = dict_sportif['prenom'],
+            sexe = dict_sportif['sexe'],
+            age = dict_sportif['age'],
+            nationalite = dict_sportif['nationalite'],
+            localisalisation = dict_sportif['localisalisation'],
+            distance_rencontre = dict_sportif['distance_rencontre'],
+            niveau_sports = dict_sportif['niveau_sports'],
+            attentes = dict_sportif['attentes'],
+            genre_recherche = dict_sportif['genre_recherche'],
+            min_age_recherchee = dict_sportif['min_age_recherchee'],
+            max_age_recherchee = dict_sportif['max_age_recherchee'],
+            photo_profil = dict_sportif['photo_profil'],
+            galerie_photos = dict_sportif['galerie_photos'],
+            nb_swipes_restantes = dict_sportif['nb_swipes_restants']
+        )
+
+
+
+func_algo_matching()
